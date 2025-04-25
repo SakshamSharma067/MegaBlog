07a3d8118d3c6eca28275ce0dfcd9630b6616cdc
SakshamSharma067 <sharma.saksham067@gmail.com>
2025-04-25 10:21:48 +0530

    add all work

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./header/header";
+import Footer from "./Footer/Footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import PostForm from "./PostForm/PostForm";
+import Input from "./Input";
+import Button from "./Button";
+import Login from "./Login";
+import AuthLayout from "./AuthLayout";
+import PostCard from "./PostCard";
+import Select from "./Select";
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    SignUp,
+    PostForm,
+    Button,
+    Input,
+    Login,
+    AuthLayout,
+    PostCard,
+    Select
+}
